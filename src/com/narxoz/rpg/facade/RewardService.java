364b8a963e92533fbfe8991cdae57098df93eac6
Dariya1317 <dariya.minamova@narxoz.kz>
2026-03-15 15:51:37 +0500

    Added reward logic based on the battle winner

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -3,9 +3,22 @@ package com.narxoz.rpg.facade;
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
         // TODO: Decide reward rules based on battle outcome.
-        if (battleResult == null) {
-            return "TODO";
+        //if (battleResult == null) {
+           // return "TODO";
+        //}
+        //return "TODO";
+        if (battleResult == null || battleResult.getWinner() == null) {
+            return "no reward (unknown outcome)";
         }
-        return "TODO";
+
+        String winner = battleResult.getWinner();
+        if (winner.equals("Draw")) {
+            return "small pouch of gold (participation prize)";
+        } else if (winner.contains("Boss") || winner.equals("TODO Boss")) {
+            return "no reward (hero was defeated)";
+        } else {
+            return "legendary dragon scale and 500 gold";
+        }
     }
+
 }
